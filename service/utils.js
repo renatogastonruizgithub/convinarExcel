f976b66a550ceb786d86c1009ed5df5ccb3d908b
unknown <renatogastonruiz.00@gmail.com>
2024-04-24 22:53:54 -0300

    refactor

--- service/utils.js
+++ service/utils.js
@@ -9,7 +9,7 @@ const XlsxPopulate = require('xlsx-populate');
 
  // Encuentra la última fila no vacía en la columna C (3)
  let lastRow = 8; // asumimos que hay al menos una fila de datos
- const columnC = sheet.column("B");
+ const columnC = sheet.column("A");
  for (let row = 8; row <= sheet.usedRange().endCell().rowNumber(); row++) {
      if (!columnC.cell(row).value() !== null) {
          lastRow = row;
@@ -17,20 +17,11 @@ const XlsxPopulate = require('xlsx-populate');
  }
 
  // Obtiene los valores desde la fila 8 hasta la última fila no vacía en la columna C
- const range = sheet.range(`B8:M${lastRow}`);
+ const range = sheet.range(`A8:M${lastRow}`);
 
  // Obtiene los valores en el rango especificado
  const values = range.value();
  
- //para indicadores
-/*  values.forEach(value => {
-    value.splice(1, 1); // Elimina el elemento en la posición 1
-    value.splice(3, 1); // Elimina el elemento en la posición 4 (ya que se eliminó un elemento previamente)
-    value.splice(3, 1); // Elimina el elemento en la posición 5 (ya que se eliminaron dos elementos previamente)
-    value.splice(3, 1); // Elimina el elemento en la posición 6 (ya que se eliminaron tres elementos previamente)
-});
- */
-
 
 const filteredUndefined = values.filter(subarray => subarray.some(item => item !== undefined));
 
@@ -71,14 +62,19 @@ async function readIndicadores(file){
  const values = range.value();
  
  //para indicadores
-  values.forEach(value => {
-    value.splice(1, 1); 
-    value.splice(3, 1); 
-    value.splice(3, 1); 
-    value.splice(3, 1); 
-});
+ values.forEach(value => {
+  // Eliminar las posiciones 1, 2, 5 y 6
+  value.splice(1, 1); // Elimina la posición 1
+  value.splice(1, 1); // Elimina la posición 2 (la nueva posición 2 es la anterior posición 3)
+  value.splice(3, 1); // Elimina la posición 5 (la nueva posición 3 es la anterior posición 4)
+  value.splice(3, 1); // Elimina la posición 6 (la nueva posición 4 es la anterior posición 5)
+ 
+});  
 
 const filteredUndefined = values.filter(subarray => subarray.some(item => item !== undefined));
+for (let i = 0; i < filteredUndefined.length; i++) {
+    filteredUndefined[i][2] = ''; // Dejar la posición 2 en vacío
+}
 
 return filteredUndefined
 } 
